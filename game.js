89147a3b6bf40dfb609e27bc1a13de3ceb51ebcf
DosState <francescobleggi@gmail.com>
2025-08-03 13:20:15 +0200

    implement game logic with IIFE

--- game.js
+++ game.js
@@ -29,3 +29,37 @@ const gameBoard = (function() {
     }
     return {getBoard, makeMove, getCurrentPlayer, reset, isFull};
 })();
+
+const gameController = (function(){
+    let gameOn = true;
+    const patterns = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8],[0,4,8],[2,4,6]]
+
+    function checkWinner(){
+        const board = gameBoard.getBoard();
+        for (let pattern of patterns){
+            const [a,b,c] = pattern;
+            if(board[a] && board[a]===board[b] && board[a]===board[c]){
+                return {winner: board[a], pattern};
+            }
+        }
+        return gameBoard.isFull() ? {winner:"tie"} : null;
+    }
+
+    function makeControlledMove(index){
+        gameBoard.makeMove(index);
+        const result = checkWinner()
+        if(result){
+            gameOn = false;
+            if (result.winner === 'tie'){
+                console.log("tie")
+            }
+            else{
+                console.log(result.winner, result.pattern)
+            }
+        }
+        else{
+            console.log(gameBoard.getCurrentPlayer)
+        }
+    }
+    return {checkWinner, makeControlledMove};
+})();
